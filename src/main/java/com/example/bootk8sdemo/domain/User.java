1e25cb9c4c6b45bc5e777967d73bea9a18f84550
Virendra Yadav <virendra.yadav@outlook.com>
2023-06-25 15:05:49 -0700

    update message

--- src/main/java/com/example/bootk8sdemo/domain/User.java
+++ src/main/java/com/example/bootk8sdemo/domain/User.java
@@ -1,2 +1,17 @@
-package com.example.bootk8sdemo.domain;public class User {
+package com.example.bootk8sdemo.domain;
+
+
+public class User {
+
+    private int userId;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    
 }
